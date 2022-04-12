a28c7347491207566d02d3b43599b34458d073b3
Fan Jing <jing1999fan@outlook.com>
2022-04-12 00:06:37 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int debugNumber = 0;
+#if DEBUG
+            Debug.Assert(debugNumber > 0);
+            Console.WriteLine("The debugNumber in debug is: " + debugNumber);
+#endif
+#if !DEBUG
+            debugNumber++;
+            Console.WriteLine("The debugNumber not in debug is: " + debugNumber);
+#endif 
+            Console.ReadKey();
+        }
+    }
+}
